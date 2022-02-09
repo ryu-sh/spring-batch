4d5346d4ae44f560a785b2499ae8b4ccc999cf8f
shryu <>
2022-02-09 23:03:07 +0900

    feat : batch sample 실행

---
+++ src/main/java/io/springbatch/springbatch/HelloJobConfiguration.java
@@ -0,0 +1,50 @@
+package io.springbatch.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class HelloJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloJob() {
+        return jobBuilderFactory.get("helloJob")
+                .start(helloStep())
+                .next(helloStep2())
+                .build();
+    }
+
+    @Bean
+    public Step helloStep() {
+        return stepBuilderFactory.get("helloStep1")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("===================");
+                    System.out.println("hello spring batch");
+                    System.out.println("===================");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Step helloStep2() {
+        return stepBuilderFactory.get("helloStep2")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("===================");
+                    System.out.println("step2 was executed");
+                    System.out.println("===================");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+}
