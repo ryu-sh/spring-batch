86fd5523d425fa6cb4975732f76ce3778ee379b9
shryu <jzxcvbn321@gmail.com>
2022-02-14 18:22:11 +0900

    feat : jobParamter 실습

--- src/main/java/io/springbatch/springbatch/JobConfiguration.java
+++ src/main/java/io/springbatch/springbatch/JobConfiguration.java
@@ -2,12 +2,15 @@ package io.springbatch.springbatch;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
@@ -28,6 +31,17 @@ public class JobConfiguration {
     public Step step1() {
         return stepBuilderFactory.get("step1")
                 .tasklet((stepContribution, chunkContext) -> {
+                    JobParameters jobParameters = stepContribution.getStepExecution().getJobParameters();
+                    jobParameters.getParameters().forEach((s, jobParameter) -> {
+                        System.out.println("paramter = " + s);
+                        System.out.println("jobParameter = " + jobParameter);
+                    });
+                    System.out.println("============================");
+                    Map<String, Object> jobParameters1 = chunkContext.getStepContext().getJobParameters();
+                    for (String s : jobParameters1.keySet()) {
+                        System.out.println("key = " + s);
+                        System.out.println(jobParameters1.get(s));
+                    }
                     System.out.println("step1 was executed");
                     return RepeatStatus.FINISHED;
                 }).build();
