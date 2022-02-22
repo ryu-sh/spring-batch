b71ef6701fb7f24ec46422a85e752b985e783e28
shryu <jzxcvbn321@gmail.com>
2022-02-22 23:26:32 +0900

    feat : simpleJob 설정

--- src/main/java/io/springbatch/springbatch/ExecutionContextConfiguration.java
+++ src/main/java/io/springbatch/springbatch/ExecutionContextConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 @RequiredArgsConstructor
 public class ExecutionContextConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
---
+++ src/main/java/io/springbatch/springbatch/job/SimpleJobConfiguration.java
@@ -0,0 +1,47 @@
+package io.springbatch.springbatch.job;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SimpleJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job simpleJob() {
+        return jobBuilderFactory.get("simpleJob")
+                .start(simpleStep())
+                .next(simpleStep2())
+                .build();
+    }
+
+    @Bean
+    public Step simpleStep() {
+        return stepBuilderFactory.get("simpleStep1")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("===================");
+                    System.out.println("hello spring batch");
+                    System.out.println("===================");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Step simpleStep2() {
+        return stepBuilderFactory.get("simpleStep2")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("===================");
+                    System.out.println("step2 was executed");
+                    System.out.println("===================");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+}
