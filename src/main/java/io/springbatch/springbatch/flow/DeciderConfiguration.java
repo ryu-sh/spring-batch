4b9046107888a96ba1e60bac0f51a77c3140fadb
shryu <jzxcvbn321@gmail.com>
2022-03-03 16:30:16 +0900

    feat : decider 예제

--- src/main/java/io/springbatch/springbatch/flow/DeciderConfiguration.java
+++ src/main/java/io/springbatch/springbatch/flow/DeciderConfiguration.java
@@ -1,48 +1,53 @@
 package io.springbatch.springbatch.flow;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 @RequiredArgsConstructor
-public class CustomExitStatusConfiguration {
+public class DeciderConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
     public Job flowJob() {
-        return jobBuilderFactory.get("flowJob")
-                .start(flowStep1())
-                .on("FAILED")
-                .to(flowStep2())
-                .on("PASS")
-                .stop()
+        return jobBuilderFactory.get("batchjob")
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .next(decider())
+                .from(decider()).on("ODD").to(oddStep())
+                .from(decider()).on("EVEN").to(evenStep())
                 .end()
                 .build();
     }
 
     @Bean
-    public Step flowStep1() {
+    public JobExecutionDecider decider() {
+        return new CustomDecider();
+    }
+
+    @Bean
+    public Step step1() {
         return stepBuilderFactory.get("step1")
                 .tasklet((stepContribution, chunkContext) -> {
                     System.out.println("step1 was executed");
-                    stepContribution.setExitStatus(ExitStatus.FAILED);
                     return RepeatStatus.FINISHED;
                 }).build();
     }
 
     @Bean
-    public Step flowStep2() {
-        return stepBuilderFactory.get("step2")
+    public Step oddStep() {
+        return stepBuilderFactory.get("oddStep")
                 .tasklet((stepContribution, chunkContext) -> {
-                    System.out.println("step2 was executed");
+                    System.out.println("oddStep was executed");
                     return RepeatStatus.FINISHED;
                 })
                 .listener(new PassCheckingListener())
@@ -50,10 +55,10 @@ public class CustomExitStatusConfiguration {
     }
 
     @Bean
-    public Step flowStep3() {
-        return stepBuilderFactory.get("step3")
+    public Step evenStep() {
+        return stepBuilderFactory.get("evenStep")
                 .tasklet((stepContribution, chunkContext) -> {
-                    System.out.println("step3 was executed");
+                    System.out.println("evenStep was executed");
                     return RepeatStatus.FINISHED;
                 }).build();
     }
