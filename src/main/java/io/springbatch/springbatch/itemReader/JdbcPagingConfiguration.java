3f2f8a425fdb2ab60f47bb0c965fb45af1f8ed7a
shryu <jzxcvbn321@gmail.com>
2022-03-17 01:58:39 +0900

    feat : JdbcPagingReader 예제

---
+++ src/main/java/io/springbatch/springbatch/itemReader/JdbcPagingConfiguration.java
@@ -0,0 +1,89 @@
+package io.springbatch.springbatch.itemReader;
+
+import io.springbatch.springbatch.chunk.Customer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.PagingQueryProvider;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class JdbcPagingConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private int chunkSize = 2;
+    private final DataSource dataSource;
+
+    @Bean
+    public Job job() throws Exception {
+        return jobBuilderFactory.get("batchjob")
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .build();
+    }
+
+    @Bean
+    public Step step1() throws Exception {
+        return stepBuilderFactory.get("step1")
+                .<Customer, Customer>chunk(chunkSize)
+                .reader(customerItemReader())
+                .writer(customItemWriter())
+                .build();
+    }
+
+    @Bean
+    public ItemReader<Customer> customerItemReader() throws Exception {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("firstname", "A%");
+
+        return new JdbcPagingItemReaderBuilder<Customer>()
+                .name("jdbcPagingItemReader")
+                .pageSize(chunkSize)
+                .dataSource(dataSource)
+                .beanRowMapper(Customer.class)
+                .queryProvider(createQueryProvider())
+                .parameterValues(parameters)
+                .build();
+    }
+
+    @Bean
+    public PagingQueryProvider createQueryProvider() throws Exception {
+        SqlPagingQueryProviderFactoryBean queryProvider = new SqlPagingQueryProviderFactoryBean();
+        queryProvider.setDataSource(dataSource);
+        queryProvider.setSelectClause("id, firstname, lastname, birthdate");
+        queryProvider.setFromClause("from customer");
+        queryProvider.setWhereClause("where firstname like :firstname");
+
+        Map<String, Order> sortKeys = new HashMap<>();
+        sortKeys.put("id", Order.ASCENDING);
+
+        queryProvider.setSortKeys(sortKeys);
+
+        return queryProvider.getObject();
+    }
+
+    @Bean
+    public ItemWriter<Customer> customItemWriter() {
+        return items -> {
+            for (Customer item : items) {
+                System.out.println("item = " + item);
+            }
+            System.out.println("end chunkSize");
+        };
+    }
+}
